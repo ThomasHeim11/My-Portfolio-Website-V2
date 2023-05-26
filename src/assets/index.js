ca48d53979f713f504db9a170ff0087e53109b72
ThomasHeim11 <wearedreamin@gmail.com>
2023-05-26 13:16:48 +0200

    removed unused code✅

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,6 +29,7 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
+
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
@@ -65,4 +66,5 @@ export {
   solidity,
   hardhat,
   chatgtp,
+ 
 };
