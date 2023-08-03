d13b5cc6846761ca1a7912d69a1f4d8adb4f98fc
ThomasHeim11 <wearedreamin@gmail.com>
2023-08-03 09:52:53 +0200

    update projects✅

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,8 +3,7 @@ import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { github } from "../assets";
-import { web3 } from "../assets";
+import { github, web3 } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -16,7 +15,7 @@ const ProjectCard = ({
   tags,
   image,
   source_code_link,
-  demo_link
+  demo_link,
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
